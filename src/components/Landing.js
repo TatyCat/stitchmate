bbf11081b237ef8cfa35c0dc65a587c8b0e08fbb
TatyCat <williams.tatyanna@gmail.com>
2019-03-17 13:59:16 -0400

    Updated class to be className. Updated Header Text

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -7,7 +7,7 @@ class Landing extends Component {
         <nav className="landing-nav">
           <h1>
             <a href="./Landing.js">
-              <span id="m-color-change"><i className="far fa-sticky-note"></i></span>StitchMate
+              <span className="m-color-change"><i className="far fa-sticky-note"></i></span>StitchMate
           </a>
           </h1>
           <ul>
@@ -15,43 +15,45 @@ class Landing extends Component {
             <li><a href="#">FAQs</a></li>
             <li id="social-media-links">
               <a href="https://pinterest.com/" target="_blank">
-                <i class="fab fa-pinterest-square"></i>
+                <i className="fab fa-pinterest-square"></i>
               </a>
               <a href="https://twitter.com/" target="_blank">
-                <i class="w3-margin-right fab fa-twitter"></i>
+                <i className="w3-margin-right fab fa-twitter"></i>
               </a>
               <a href="https://facebook.com/" target="_blank">
-                <i class="w3-margin-right fab fa-facebook"></i>
+                <i className="w3-margin-right fab fa-facebook"></i>
               </a>
             </li>
 
           </ul>
         </nav>
-        <main class="main-container landing-pg">
+        <main className="main-container landing-pg">
           <header>
             <h2><span>Knit? Crochet?  </span>
               Need to keep count of your sitches?</h2>
-            <h1>Keep track of your craft and stitch with us!</h1>
+            <h1>Keep track by stitching with us
+
+            </h1>
             <button><span>Register / Log In</span></button>
           </header>
           <article>
-            <h3>The <i class="far fa-sticky-note"></i>StitchMate Way</h3>
+            <h3>The <span className="stitchmate-post-it"><i className="far fa-sticky-note"></i></span><span className="stitchmate-logo-lettering">StitchMate</span> Way</h3>
             <section>
               <ul>
                 <li>
-                  <h2><i class="fas fa-palette"></i></h2>
+                  <h2><i className="fas fa-palette"></i></h2>
                   <p>Add Custom Knit and Crochet Patterns</p>
                 </li>
                 <li>
-                  <h2><i class="fas fa-magic"></i></h2>
+                  <h2><i className="fas fa-magic"></i></h2>
                   <p>As Easy as 1...2...3 <br /> keep  track of where you left off</p>
                 </li>
                 <li>
-                  <h2><i class="fas fa-map-pin"></i></h2>
+                  <h2><i className="fas fa-map-pin"></i></h2>
                   <p>Never lose your place in your pattern again</p>
                 </li>
                 <li>
-                  <h2><i class="far fa-clock"></i></h2>
+                  <h2><i className="far fa-clock"></i></h2>
                   <p>Organize and save time</p>
                 </li>
               </ul>
