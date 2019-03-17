b256aec1b18a388abe6705011e43599fc0b842d0
TatyCat <williams.tatyanna@gmail.com>
2019-03-17 15:31:39 -0400

    Changed the landing memos to be centered. padding bug

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,14 @@ class Footer extends Component {
         <footer>
           <p>Contact - Follow - Donate:</p>
           <p>
-            <a href="https://twitter.com/DevTaty" target="_blank"><i class="w3-margin-right fab fa-twitter"></i></a>
-            <a href="https://www.linkedin.com/in/tatyannac/" target="_blank"><i class="w3-margin-right fab fa-linkedin-in"></i></a>
-            <a href="mailto:tatyanna.cobb@gmail.com" target="_blank"><i class="w3-margin-right fas fa-envelope"></i></a>
-            <a href="https://paypal.me/DevTatyanna"><i class="fas fa-donate"></i>
+            <a href="https://twitter.com/DevTaty" target="_blank"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.linkedin.com/in/tatyannac/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+            <a href="mailto:tatyanna.cobb@gmail.com" target="_blank"><i class=" fas fa-envelope"></i></a>
+            <a href="https://paypal.me/DevTatyanna" target="_blank"><i class="fas fa-donate"></i>
             </a>
           </p>
 
-          <p class="w3-small w3-wide">Copyright &copy;2018 Tatyanna Cobb. All rights reserved. </p>
+          <p>Copyright &copy;2018 Tatyanna Cobb. All rights reserved. </p>
         </footer>
 
       </>
